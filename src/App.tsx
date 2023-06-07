ef62adec255f86e7762736ddf0c180346604970d
Alper alanyalie <30805005+alperalanyali@users.noreply.github.com>
2023-06-07 17:47:36 +0300

    App tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
-import {SafeAreaView, Text, View} from 'react-navite';
+import {FlatList, SafeAreaView, StyleSheet, Text, View} from 'react-native';
 
 import React from 'react';
+import SongCard from './components/SongCard/SongCard';
+import music_data from './music-data.json';
 
 const App = () => {
+
+  const renderMusic = ({item}) => <SongCard song={item}/>
   return (
-    <SafeAreaView>
-      <View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
         <Text>Music App</Text>
+        <FlatList
+          data={music_data}
+          renderItem={renderMusic}
+        />
       </View>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1
+  }
+
+});
 
 export default App ;
