8b01e8dc5af3e4887f873760f82a3c87b548171f
Alper alanyalie <30805005+alperalanyali@users.noreply.github.com>
2023-06-07 17:11:55 +0300

    Main folders are added

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import {SafeAreaView, Text, View} from 'react-navite';
+
+import React from 'react';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Music App</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default App ;
